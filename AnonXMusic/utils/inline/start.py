164fc16b2c1bf6486098a3fee940fc0159ed7619
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-08-31 16:17:32 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -19,8 +19,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),   
-        ]
-        [
+
             InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
     ]
